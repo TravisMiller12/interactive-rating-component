86b98882cd1cd7e05b3c6cb16d25b195c4516756
Travis Miller <travismiller12@gmail.com>
2024-02-21 22:15:27 +0200

    Update styles.css

---
+++ styles.css
@@ -0,0 +1,24 @@
+.rating-buttons{
+    display: flex;
+}
+
+h1{
+    font-family: "Overpass", sans-serif;
+    font-weight: 700;
+    color: white;
+}
+
+.container{
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+    border-radius: 20px;
+    background: linear-gradient(to bottom, #242833, #181E25);
+}
+
+p{
+    font-size: 15px;
+    font-family: "Overpass", sans-serif;
+    font-weight: 400;
+    color: hsl(217, 12%, 63%);
+}
